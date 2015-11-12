ece27f22fb72f512c4c889d35c9cec565eb794dd
Ovidiu Porumb <ovidiu.porumb@live.com>
2015-11-13 00:24:55 +0200

    added basic car model

---
+++ Backbone.Playground/Models/Car.js
@@ -0,0 +1,9 @@
+﻿var Car = Backbone.showModelessDialog.extend({
+    defaults: {
+        make: '',
+        model: '',
+        yearOfMaking: 0,
+        horsePower: 0,
+        gearBox: ''
+    }
+});
